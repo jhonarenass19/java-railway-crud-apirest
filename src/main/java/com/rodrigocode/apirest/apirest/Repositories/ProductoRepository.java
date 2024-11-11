679412cea91bdc313a560e27ed08c6b81c0fc734
rodrigo <jhonrodrigoarenass@gmail.com>
2024-11-11 14:58:30 -0500

    Hicimos una version configurada de nuestro proyecto API REST  CRUD con POSTGRES para poder desplegar

---
+++ src/main/java/com/rodrigocode/apirest/apirest/Repositories/ProductoRepository.java
@@ -0,0 +1,9 @@
+package com.rodrigocode.apirest.apirest.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rodrigocode.apirest.apirest.Entities.Producto;
+
+public interface ProductoRepository extends JpaRepository<Producto, Long > {
+
+}
